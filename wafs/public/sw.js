651f76b7d3fafbd7f89aef90892c63e25ad3301e
dorusth <dorusth@hotmail.com>
2019-03-28 09:27:59 +0100

    service worker, webp images

---
+++ wafs/public/sw.js
@@ -0,0 +1,45 @@
+var CACHE_NAME = 'github static';
+var urlsToCache = [
+  './css/styles.css',
+  './css/bootstrap.css',
+  './js/jquery-3.3.1.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('load', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
